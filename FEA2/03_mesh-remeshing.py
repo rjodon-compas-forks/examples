3db7d1feed6afb2682e1b744b8aea30f58c87b87
brgbits <brg@arch.ethz.ch>
2020-02-26 14:49:08 +0100

    flake

--- FEA2/03_mesh-remeshing.py
+++ FEA2/03_mesh-remeshing.py
@@ -14,13 +14,14 @@ key = mesh.insert_vertex(0)
 fixed = [key]
 
 plotter = MeshPlotter(mesh, figsize=(8, 5))
-
 plotter.draw_edges(width=0.5)
+
 
 def callback(mesh, k, args):
     print(k)
     plotter.update_edges()
     plotter.update()
+
 
 trimesh_remesh(
     mesh,
--- FEA2/04_topop.py
+++ FEA2/04_topop.py
@@ -7,9 +7,11 @@ nely = 40
 plt.figure(figsize=(8, 5))
 plt.axis([0, nelx, 0, nely])
 
+
 def callback(x):
     plt.imshow(1 - x, cmap='gray', origin='lower')
     plt.pause(0.001)
+
 
 loads = {'200-40': [0, -1]}
 supports = {'0-0': [1, 1], '400-0': [0, 1]}
